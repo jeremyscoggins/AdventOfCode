736a470c35903307998c026d43ceec80b62f36f4
Jeremy Scoggins <jscoggin@hotmail.com>
2015-12-14 17:18:03 -0700

    added code for day 13 and day 14

---
+++ Day13/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    class Program
+    {
+        const string pattern = "(\\w+) would (\\w+) (\\d+) happiness units by sitting next to (\\w+)\\.";
+        static Dictionary<string, int> seatingArrangments = new Dictionary<string, int>();
+        static Dictionary<string, Dictionary<string, int>> seatingValues = new Dictionary<string, Dictionary<string, int>>();
+        static void Walk(string last, int value, string arrangement)
+        {
+            bool finished = true;
+            foreach (var next in seatingValues.Keys)
+            {
+                if (!arrangement.Contains(next))
+                {
+                    finished = false;
+                    int nextValue = value;
+                    nextValue = nextValue + seatingValues[last][next];
+                    nextValue = nextValue + seatingValues[next][last];
+
+                    Walk(next, nextValue, arrangement + " " + next);
+                }
+            }
+            if (finished)
+            {
+                string[] names = arrangement.Split(' ');
+                value = value + seatingValues[names[0]][names[names.Length - 1]];
+                value = value + seatingValues[names[names.Length - 1]][names[0]];
+                seatingArrangments[arrangement] = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead("input.txt"))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        Match match = Regex.Match(line, pattern);
+                        string nameA = match.Groups[1].Value;
+                        string direction = match.Groups[2].Value;
+                        int value = int.Parse(match.Groups[3].Value);
+                        string nameB = match.Groups[4].Value;
+                        if(!seatingValues.ContainsKey(nameA))
+                        {
+                            seatingValues[nameA] = new Dictionary<string,int>();
+                        }
+                        seatingValues[nameA][nameB] = direction == "gain" ? value : -value;
+                    }
+                }
+            }
+            foreach (var name in seatingValues.Keys.ToArray())
+            {
+                seatingValues[name]["Me"] = 0;
+                if (!seatingValues.ContainsKey("Me"))
+                {
+                    seatingValues["Me"] = new Dictionary<string, int>();
+                }
+                seatingValues["Me"][name] = 0;
+            }
+            foreach (var name in seatingValues.Keys)
+            {
+                Walk(name, 0, name);
+            }
+            var best = seatingArrangments.OrderByDescending(a => a.Value).First();
+            Console.WriteLine("the best seating arrangement was\n({0})\nwith a total happiness gain of {1}", best.Key, best.Value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    class Reindeer
+    {
+        public string Name;
+        public int Speed;
+        public int EnduranceTime;
+        public int RestTime;
+        public int CurrentTime;
+        public bool Resting;
+        public int Distance;
+        public int Points;
+    }
+    class Program
+    {
+        const string pattern = "(\\w+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.";
+        static void Main(string[] args)
+        {
+            List<Reindeer> reindeer = new List<Reindeer>();
+            using (var fileStream = File.OpenRead("input.txt"))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        Match match = Regex.Match(line, pattern);
+                        string name = match.Groups[1].Value;
+                        int speed = int.Parse(match.Groups[2].Value);
+                        int endurance = int.Parse(match.Groups[3].Value);
+                        int rest = int.Parse(match.Groups[4].Value);
+                        reindeer.Add(new Reindeer() { Name = name, EnduranceTime = endurance, Speed = speed, RestTime = rest, CurrentTime = 0, Resting = false, Distance = 0, Points = 0 });
+                    }
+                }
+            }
+            for (int i = 0; i < 2503; i++)
+            {
+                foreach (var single in reindeer)
+                {
+                    single.CurrentTime++;
+                    if (!single.Resting)
+                    {
+                        single.Distance += single.Speed;
+                    }
+                    if (single.Resting && single.CurrentTime >= single.RestTime)
+                    {
+                        single.Resting = false;
+                        single.CurrentTime = 0;
+                    }
+                    else if (!single.Resting && single.CurrentTime >= single.EnduranceTime)
+                    {
+                        single.Resting = true;
+                        single.CurrentTime = 0;
+                    }
+                }
+                var leadDist = reindeer.Max(r => r.Distance);
+                foreach (var single in reindeer)
+                {
+                    if (single.Distance == leadDist)
+                    {
+                        single.Points++;
+                    }
+                }
+            }
+            var bestDist = reindeer.OrderByDescending(a => a.Distance).First();
+            Console.WriteLine("{0} was the fastest Reindeer with a distance of {1}", bestDist.Name, bestDist.Distance);
+            var bestPoints = reindeer.OrderByDescending(a => a.Points).First();
+            Console.WriteLine("{0} earned the most points with a score of {1} points", bestPoints.Name, bestPoints.Points);
+            Console.ReadLine();
+        }
+    }
+}
